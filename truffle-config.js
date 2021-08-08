0fd1a12c03c63befa77375ce6a9a63a792ec1a89
vvronskyFX <vronskymail@gmail.com>
2021-08-08 12:24:48 -0400

    Dex Updates and Changes

--- truffle-config.js
+++ truffle-config.js
@@ -1,15 +1,28 @@
 require('babel-register');
 require('babel-polyfill');
 require('dotenv').config();
+const HDWalletProvider = require('truffle-hdwallet-provider-privkey');
+const privateKeys = process.env.PRIVATE_KEYS || ""
 
 module.exports = {
  
   networks: {
     development: {
-      host: "127.0.0.1",
-      port: 9545,
+      host: "192.168.99.80",
+      port: 7545,
       network_id: "*" //Match and Network id
     },
+    kovan: {
+      provider: function() {
+        return new HDWalletProvider(
+          privateKeys.split(','), // Array of account private keys
+          `https://kovan.infura.io/v3/${process.env.INFURA_API_KEY}` // Url to an Ethereum Node
+        )
+      },
+      gas: 5000000,
+      gasPrice: 25000000000,
+      network_id: 42
+    }
   },
   contracts_directory: './src/contracts/',
   contracts_build_directory: './src/abis/',
@@ -21,4 +34,4 @@ module.exports = {
         }
       }
     }
-  }
+  };
